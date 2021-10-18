f448b27b955d47df7e75ed9bd1aa2a68db277f48
Nguyen Minh Phuc <nmphuc@outlook.com>
2021-10-18 07:38:08 +0700

    những thây đổi của buổi trước

--- ThucHanh_Solution20212022/Chapter03/Project_QuaySoMayMan/ClsMain.cs
+++ ThucHanh_Solution20212022/Chapter03/Project_QuaySoMayMan/ClsMain.cs
@@ -10,5 +10,8 @@ namespace Project_QuaySoMayMan
    public class ClsMain
     { 
         public static string pathGiaiThuong = Application.StartupPath+@"\Giai.ini";
+        public static string pathNhanVienNhanGiai= Application.StartupPath + @"\NhanVienNhanGiai.ini";
+        public static string pathNhanVienNhanGiaiExcel = Application.StartupPath + @"\NhanVienNhanGiai.xls";
+        public static string pathNhanVien = Application.StartupPath + @"\NhanVien1.xls";
     }
 }
---
+++ ThucHanh_Solution20212022/Chapter03/Project_QuaySoMayMan/Frm_QuanLyNhanVienTrungThuong_Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_QuaySoMayMan
+{
+    public partial class Frm_QuanLyNhanVienTrungThuong_Main : Form
+    {
+        public Frm_QuanLyNhanVienTrungThuong_Main()
+        {
+            InitializeComponent();
+        }
+       
+        private void Frm_QuanLyNhanVienTrungThuong_Main_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnRefress_Click(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+
+        private void DisplayData()
+        {
+            
+
+         
+            
+        }
+    }
+}
